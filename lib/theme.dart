672d89c17c5719fc898dba784d00c4baad9c4f6b
Mohammed Anwar <84952928+MohammedAnwar2@users.noreply.github.com>
2023-05-13 12:42:52 +0530

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,10 +3,19 @@ import 'package:flutter/material.dart';
 class Theam {
   ThemeData DarkMode = ThemeData.dark().copyWith(
     appBarTheme: AppBarTheme(color: Colors.brown),
-    textTheme: TextTheme(displayLarge:TextStyle(color: Colors.amberAccent) )
+    textTheme: TextTheme(displayLarge:TextStyle(color: Colors.lightGreen,fontSize: 20,fontWeight: FontWeight.bold) )
   );
   ThemeData LightMode = ThemeData.dark().copyWith(
     appBarTheme: AppBarTheme(color: Colors.deepPurple),
-    textTheme: TextTheme(displayLarge:TextStyle(color: Colors.blue) )
+    textTheme: TextTheme(displayLarge:TextStyle(color: Colors.deepOrange,fontSize: 20,fontWeight: FontWeight.bold) )
   );
+}
+
+DarkText(BuildContext context)
+{
+  return Theme.of(context).textTheme.displayLarge;
+}
+LightText(BuildContext context)
+{
+  return Theme.of(context).textTheme.displayLarge;
 }
